ded469ae8fc73ae363fc36637608eb731fd18db5
Mauricio <Mauricio>
2016-09-01 23:41:19 -0300

    Code Comments

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,9 +3,15 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * @author Mauricio F. Caldas Esteves
+ */
+@SpringBootApplication //This annotation is the same as using @Configuration @EnableAutoConfiguration @ComponentScan.
 public class Application {
 
+	/**
+	 * Main method to launch application.
+	 */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,10 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * HTTP requests are handled by a controller. 
+ * This controller will handle GET requests for /greeting by returing the name of a View (greeting).
+ * A View is responsible for rendering the HTML content.
+ */
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+	/**
+	 * @RequestMapping - ensures that HTTP requests to /greeting are mapped to greeting() method;
+	 * @RequestParam - binds the value of the query String parameter name into the name parameter of the greeting() method;
+	 */
+    @RequestMapping("/greeting") //ensures that HTTP requests to /greeting are mapped to greeting() method
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
